aaa131e6e302528c70903afa1ed8d1ef9c9f4dba
h3m5t <henriquee.trajanoo@gmail.com>
2025-07-06 18:52:28 -0300

    projeto com a logica quase pronta, usei IA p/ mexer no css

--- src/app/calcula-media/calcula-media.ts
+++ src/app/calcula-media/calcula-media.ts
@@ -1,68 +1,89 @@
-
 import { Component } from '@angular/core';
-import { Situacao } from './situacao'; 
-import { Boletim } from './Boletim'; 
+import { Situacao } from './situacao';
+import { Boletim } from './Boletim';
 
 @Component({
   selector: 'app-calcula-media',
   standalone: false,
   templateUrl: './calcula-media.html',
-  styleUrl: './calcula-media.css'
+  styleUrls: ['./calcula-media.css'] // Corrigido para styleUrls
 })
 export class CalculaMedia {
-  notaB1: number
-  notaB2: number 
-  notaB3: number 
-  notaB4: number 
-  ProvaFinal: number
+  notaB1: number;
+  notaB2: number;
+  notaB3: number;
+  notaB4: number;
+  ProvaFinal: number;
 
-  mediaParcial: number | undefined
-  situacaoParcial: Situacao 
-  situacaoFinal: Situacao
-  mediaFinal: number
+  mediaParcial: number | undefined;
+  situacaoParcial: Situacao;
+  mediaFinal: number | undefined; 
+  situacaoFinal: Situacao;
 
-  private boletim: Boletim
+  private boletim: Boletim;
+
+  Situacao = Situacao
 
   constructor() {
-    this.mediaParcial = undefined
-    this.situacaoParcial = Situacao.CURSANDO
-    this.boletim = new Boletim()
-    this.notaB1 = 0
-    this.notaB2 = 0
-    this.notaB3 = 0
-    this.notaB4 = 0
-    this.ProvaFinal = 0
-    this.situacaoFinal = Situacao.INDETERMINADO
-    this.mediaFinal = 0
+    this.boletim = new Boletim();
+    this.notaB1 = 0;
+    this.notaB2 = 0;
+    this.notaB3 = 0;
+    this.notaB4 = 0;
+    this.ProvaFinal = 0;
+
+    this.mediaParcial = undefined;
+    this.situacaoParcial = Situacao.CURSANDO;
+    this.mediaFinal = undefined;
+    this.situacaoFinal = Situacao.INDETERMINADO;
   }
 
+ 
+  CalcularTudo() {
+    this.ProcessarMediaParcialESituacao();
+    this.ProcessarSituacaoFinal();
+  }
 
-  CalculoParcial() {
-    if (this.notaB1 != undefined && this.notaB2 != undefined &&
-        this.notaB3 != undefined && this.notaB4 != undefined) {
+  private ProcessarMediaParcialESituacao() {
+    this.mediaFinal = undefined;
+    this.situacaoFinal = Situacao.INDETERMINADO;
 
-      this.mediaParcial = this.boletim.calcularMediaPonderada(
+
+    if (this.notaB1 >= 0 && this.notaB2 >= 0 && this.notaB3 >= 0 && this.notaB4 >= 0) {
+      this.mediaParcial = this.boletim.CalcularMediaPonderada(
         this.notaB1, this.notaB2, this.notaB3, this.notaB4
       );
-      this.situacaoParcial = this.boletim.determinarSituacao(this.mediaParcial);
-
+      this.situacaoParcial = this.boletim.DeterminarSituacaoParcial(this.mediaParcial);
     } else {
       this.mediaParcial = undefined;
       this.situacaoParcial = Situacao.CURSANDO;
     }
   }
 
-CalculoMediaFinal(){
-  if(this.mediaParcial != undefined){
-    this.mediaFinal = (this.mediaParcial + this.ProvaFinal)/2 
-  }
-}
+  private ProcessarSituacaoFinal() {
+    if (this.mediaParcial == undefined) {
+      return;
+    }
 
-  CalculoFinal(){
-    if(this.mediaParcial != undefined && (this.mediaFinal >=60)){
-      this.situacaoFinal = Situacao.APROVADO
-    }else {
-      this.situacaoFinal = Situacao.REPROVADO
+    if (this.situacaoParcial == Situacao.APROVADO) {
+      this.situacaoFinal = Situacao.APROVADO;
+      this.mediaFinal = this.mediaParcial; 
+    } else if (this.situacaoParcial === Situacao.AVFINAL) {
+      if (this.ProvaFinal >= 0) { 
+        this.mediaFinal = this.boletim.CalcularMediaFinalAposProvaFinal(this.mediaParcial, this.ProvaFinal);
+        
+        if (this.mediaFinal >= 60) {
+          this.situacaoFinal = Situacao.APROVADO;
+        } else {
+          this.situacaoFinal = Situacao.REPROVADO;
+        }
+      } else {
+        this.situacaoFinal = Situacao.INDETERMINADO;
+        this.mediaFinal = undefined;
+      }
+    } else if (this.situacaoParcial === Situacao.REPROVADO) {
+      this.situacaoFinal = Situacao.REPROVADO;
+      this.mediaFinal = this.mediaParcial; 
     }
   }
 }
